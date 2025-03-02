d0c692de622577bbc0282cd7f2c6e49eca2cb092
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-02 14:56:53 +0000

    escape the glob pattern properly

--- src/data_extractors/metadata.ts
+++ src/data_extractors/metadata.ts
@@ -1,31 +1,31 @@
 import { EntityMetadataCollection } from '@radixdlt/babylon-gateway-api-sdk';
 
 export function extractStringMetadata(
-  metadataItems: EntityMetadataCollection,
-  key: string
+    metadataItems: EntityMetadataCollection,
+    key: string
 ): string | null {
-  const item = metadataItems.items.find((item) => item.key == key);
-  if (!item) return null;
-  if (item.value.typed.type !== 'String') return null;
-  return item.value.typed.value;
+    const item = metadataItems.items.find((item) => item.key == key);
+    if (!item) return null;
+    if (item.value.typed.type !== 'String') return null;
+    return item.value.typed.value;
 }
 
 export function extractUrlMetadata(
-  metadataItems: EntityMetadataCollection,
-  key: string
+    metadataItems: EntityMetadataCollection,
+    key: string
 ): string | null {
-  const item = metadataItems.items.find((item) => item.key == key);
-  if (!item) return null;
-  if (item.value.typed.type !== 'Url') return null;
-  return item.value.typed.value;
+    const item = metadataItems.items.find((item) => item.key == key);
+    if (!item) return null;
+    if (item.value.typed.type !== 'Url') return null;
+    return item.value.typed.value;
 }
 
 export function extractStringArrayMetadata(
-  metadataItems: EntityMetadataCollection,
-  key: string
+    metadataItems: EntityMetadataCollection,
+    key: string
 ): string[] | null {
-  const item = metadataItems.items.find((item) => item.key == key);
-  if (!item) return null;
-  if (item.value.typed.type !== 'StringArray') return null;
-  return item.value.typed.values;
+    const item = metadataItems.items.find((item) => item.key == key);
+    if (!item) return null;
+    if (item.value.typed.type !== 'StringArray') return null;
+    return item.value.typed.values;
 }
