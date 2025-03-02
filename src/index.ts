3aad2dee20eb11f8c7b319482f0a6665b5124e22
Krulknul <85589076+Krulknul@users.noreply.github.com>
2025-03-02 14:52:07 +0000

    format all the stuff

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,21 @@
-import { GatewayApiClient } from "@radixdlt/babylon-gateway-api-sdk"
-import { Account } from "./account"
-
+import { GatewayApiClient } from '@radixdlt/babylon-gateway-api-sdk';
+import { Account } from './account';
 
 // Please add methods to this class to extend the functionality
 export class GatewayEzMode {
-    gateway: GatewayApiClient
+    gateway: GatewayApiClient;
     constructor(gateway?: GatewayApiClient) {
         if (gateway) {
-            this.gateway = gateway
+            this.gateway = gateway;
         } else {
             this.gateway = GatewayApiClient.initialize({
                 applicationName: '',
                 networkId: 1,
-            })
+            });
         }
     }
 
     getAccount(address: string): Account {
-        return new Account(address, this.gateway)
+        return new Account(address, this.gateway);
     }
 }
